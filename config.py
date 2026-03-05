f2a488ab1a92e9a8de42b4ad62b00a1071bdcca1
NEXUM <tgaziz050@gmail.com>
2026-03-06 03:33:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Gemini keys
 GEMINI_KEYS = [
     os.getenv("GEMINI_1"),
     os.getenv("GEMINI_2"),
@@ -15,7 +14,6 @@ GEMINI_KEYS = [
     os.getenv("GEMINI_6"),
 ]
 
-# Groq keys
 GROQ_KEYS = [
     os.getenv("GROQ_1"),
     os.getenv("GROQ_2"),
@@ -24,7 +22,5 @@ GROQ_KEYS = [
     os.getenv("GROQ_5"),
     os.getenv("GROQ_6"),
 ]
-
-OPENAI_KEY = os.getenv("OPENAI_KEY")
 
 ADMIN_USER_ID = os.getenv("ADMIN_USER_ID")

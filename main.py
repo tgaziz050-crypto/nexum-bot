bcf442cb77f814289e91aff9d413bf2e87f74f5d
NEXUM <tgaziz050@gmail.com>
2026-03-06 04:29:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from telegram.bot import start_polling
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import BOT_TOKEN
+from telegram.handlers import router
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+dp.include_router(router)
+
+
+async def main():
+
+    print("NEXUM v3 STARTED")
+
+    await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(start_polling())
+    asyncio.run(main())
